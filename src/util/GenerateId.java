714a048b04c4741c8030b7ce249c6a7bf1364dc3
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-28 17:03:20 -0300

    feat: util GenerateId

---
+++ src/util/GenerateId.java
@@ -0,0 +1,17 @@
+package util;
+
+public class GenerateId {
+    private int id;
+    private final static GenerateId instance = new GenerateId();
+
+    private GenerateId() {}
+
+    public static GenerateId getInstance() {
+        return instance;
+    }
+
+    public int getId() {
+        return ++id;
+    }
+
+}

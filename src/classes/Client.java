c68e31f8abd65c40de2a77738b4c6dd76e972882
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-28 17:23:51 -0300

    refact: Client id being passed by constructor

--- src/classes/Client.java
+++ src/classes/Client.java
@@ -6,12 +6,10 @@ public class Client {
     private final int id;
     private final String cpf, name;
 
-    public Client(String cpf, String name) {
+    public Client(int id, String cpf, String name) {
+        this.id = id;
         this.cpf = cpf;
         this.name = name;
-
-        GenerateId generateId = GenerateId.getInstance();
-        this.id = generateId.getId();
     }
 
     @Override

2a926f30f71e3e59a310ba14b6a95e87cdb15e96
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-28 18:05:04 -0300

    feat: method createSpecialAccount created

--- src/data/AccountManager.java
+++ src/data/AccountManager.java
@@ -3,13 +3,14 @@ package data;
 import classes.Account;
 import classes.CheckingAccount;
 import classes.Client;
+import classes.SpecialAccount;
 import util.GenerateId;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AccountManager {
-    private GenerateId generateId;
+    private final GenerateId generateId;
     private Map<Integer, Account> accounts;
 
     public AccountManager() {
@@ -20,6 +21,12 @@ public class AccountManager {
     public void createCheckingAccount(Client client) {
         int idAccount = generateId.getId();
         CheckingAccount account = new CheckingAccount(idAccount, client);
+        accounts.put(idAccount, account);
+    }
+
+    public void createSpecialAccount(Client client) {
+        int idAccount = generateId.getId();
+        SpecialAccount account = new SpecialAccount(idAccount, client, 500);
         accounts.put(idAccount, account);
     }
 
